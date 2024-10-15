c15e30ab0689861ffde23fc4ba89b5036e1aabd4
Fernando Morata <fernando-5-vva@hotmail.com>
2024-10-15 20:16:17 +0200

    añadido mensaje de activación

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-// server.js
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-// Controladores
 const {
     newUserController,
     getUserController,
@@ -30,25 +28,17 @@ const {
 const { authUser } = require('./middlewares/auth');
 
 const app = express();
-
-// Middleware
-
-app.use(
-    cors({
-        origin: 'http://localhost:3000', // Asegúrate de que sea la URL correcta de tu frontend
-    })
-);
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-// Conexión a MongoDB
-mongoose
-    .connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log('MongoDB conectado'))
-    .catch((err) => console.error('Error al conectar a MongoDB:', err));
+// Ruta para comprobar si el servidor está funcionando
+app.get('/', (req, res) => {
+    res.send({
+        status: 'success',
+        message: 'Servidor funcionando correctamente',
+    });
+});
 
 // Rutas de usuario
 app.post('/user', newUserController);
@@ -56,9 +46,13 @@ app.get('/user/:id', authUser, getUserController);
 app.post('/login', loginController);
 app.get('/user', authUser, getMeController);
 app.get('/user/:id/links', getUserLinksController);
+// Modificar email o username
 app.put('/user/edit', authUser, editUser);
+// Modificar la biografía del usuario
 app.put('/user/bio', authUser, editBio);
+// Modificar la contraseña del usuario
 app.put('/users/password', authUser, editUserPass);
+// Eliminar un usuario
 app.delete('/users', authUser, deleteUser);
 
 // Rutas de Links
@@ -69,6 +63,21 @@ app.delete('/link/:id', authUser, deleteLinkController);
 
 // Rutas de votos
 app.post('/votos/:idLink', authUser, addVotes);
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
+    );
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 // Middleware de 404
 app.use((req, res) => {
@@ -88,7 +97,7 @@ app.use((error, req, res, next) => {
 });
 
 // Lanzamos el servidor
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-    console.log(`Servidor funcionando perfectamente en el puerto ${PORT} 🤩`);
+const puerto = process.env.PORT || 4000; // Cambiar para usar el puerto de la variable de entorno
+app.listen(puerto, () => {
+    console.log(`Servidor funcionando perfectamente en el puerto ${puerto} 🤩`);
 });
