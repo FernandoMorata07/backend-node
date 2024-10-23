e5cb8622d8d659961d77a4d7bf7b3badd9b01edb
Fernando Morata <fernando-5-vva@hotmail.com>
2024-10-23 18:47:25 +0200

    revisar cambios

--- server.js
+++ server.js
@@ -41,33 +41,44 @@ app.get('/', (req, res) => {
     });
 });
 
-// Middleware de autenticación solo para rutas que lo requieren
-app.use('/user', authUser); // Solo se aplica a las rutas que comienzan con /user
-app.use('/link', authUser); // Solo se aplica a las rutas que comienzan con /link
-
 // Rutas de usuario
-app.post('/user', newUserController);
-app.get('/user/:id', getUserController);
+app.post('/register', newUserController); // Ruta para registrar un nuevo usuario
+app.get('/user/:id', authUser, getUserController);
 app.post('/login', loginController);
-app.get('/user', getMeController);
+app.get('/user', authUser, getMeController);
 app.get('/user/:id/links', getUserLinksController);
 // Modificar email o username
-app.put('/user/edit', editUser);
+app.put('/user/edit', authUser, editUser);
 // Modificar la biografía del usuario
-app.put('/user/bio', editBio);
+app.put('/user/bio', authUser, editBio);
 // Modificar la contraseña del usuario
-app.put('/users/password', editUserPass);
+app.put('/users/password', authUser, editUserPass);
 // Eliminar un usuario
-app.delete('/users', deleteUser);
+app.delete('/users', authUser, deleteUser);
 
 // Rutas de Links
-app.post('/link', newLinkController);
-app.get('/link', getLinksController);
-app.get('/link/:id', getSingleLinkController);
-app.delete('/link/:id', deleteLinkController);
+app.post('/links', authUser, newLinkController); // Corregido para usar "/links"
+app.get('/links', authUser, getLinksController); // Corregido para usar "/links"
+app.get('/link/:id', authUser, getSingleLinkController);
+app.delete('/link/:id', authUser, deleteLinkController);
 
 // Rutas de votos
-app.post('/votos/:idLink', addVotes);
+app.post('/votos/:idLink', authUser, addVotes);
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
+    );
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 // Middleware de 404
 app.use((req, res) => {
